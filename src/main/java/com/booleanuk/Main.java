3b559cb002e32e48392e671e2d2f61221b3f86ac
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-04-28 18:32:56 +0300

    test

--- src/main/java/com/booleanuk/Main.java
+++ src/main/java/com/booleanuk/Main.java
@@ -3,6 +3,7 @@ package com.booleanuk;
 public class Main {
     public static void main(String[] args) {
 
+
         Quiz quiz = Quiz.getInstance();
         quiz.createSampleQuestions();
         quiz.run();
