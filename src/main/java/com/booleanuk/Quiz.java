56b368a6795643ffcf751b72102e7df60b979be9
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-03 02:23:28 +0300

    Refactored code, user io separated

--- src/main/java/com/booleanuk/Quiz.java
+++ src/main/java/com/booleanuk/Quiz.java
@@ -8,7 +8,6 @@ import java.util.*;
 public class Quiz {
     private List<Question> questions = new ArrayList<>();
     private static Quiz instance;
-    private final Scanner scanner = new Scanner(System.in);
 
     public static Quiz getInstance() {
         if (instance == null) {
@@ -17,28 +16,21 @@ public class Quiz {
         return instance;
     }
 
-    public void addQuestions(Question... questions) {
-        this.questions.addAll(Arrays.asList(questions));
-    }
-
     public void run() {
-        String welcomeText = """
+        UserInterface.showMessage("""
                 Welcome to the best Quiz!
-                Would you like to give it a try?(y/n)""";
-        System.out.println(welcomeText);
-
+                Would you like to give it a try?(Y/N)""");
         while (true) {
             try {
-                char start = scanner.nextLine().trim().toLowerCase().charAt(0);
-                if (start == 'y') {
-                    loadQuestions();
+                char userInput = UserInterface.getUserInput();
+                if (userInput == 'Y') {
                     startQuiz();
                     break;
-                } else if (start == 'n') {
-                    System.out.println("\nExiting quiz");
+                } else if (userInput == 'N') {
+                    UserInterface.showMessage("Exiting Quiz. See you next time!");
                     break;
                 } else {
-                    System.out.println("Please enter y for yes or n for no");
+                    UserInterface.showMessage("Please enter Y for Yes or N for No");
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -47,93 +39,57 @@ public class Quiz {
     }
 
     public void startQuiz() {
-        String quizExplanation = "Please answer the following questions:";
-        System.out.println("\n" + quizExplanation);
+        loadQuestions();
 
         int questionsAsked = 0;
         int correctAnswers = 0;
 
         for (Question question : questions) {
-            // Print Question and Possible Answers
-            System.out.println(question.getQuestion());
+            // Print Question
+            UserInterface.showMessage(question.getQuestion());
+            questionsAsked++;
 
-            // Map letter to answer and print
+            // Map a letter to each answer and show them to the user
             char key = 'A';
             Map<Character, String> possibleAnswersMap = new HashMap<>();
             for (String possibleAnswer : question.getPossibleAnswers()) {
                 possibleAnswersMap.put(key++, possibleAnswer);
             }
-            possibleAnswersMap.forEach((k, v) -> System.out.println(k + ": " + v));
+            possibleAnswersMap.forEach((k, v) -> UserInterface.showMessage(k + ": " + v));
 
-            // Get user input
-            char selection = '0';
+            // Check if the answer is correct and respond with appropriate message
             try {
-                selection = scanner.nextLine().trim().toUpperCase().charAt(0);
+                char userInput = UserInterface.getUserInput();
+                if (possibleAnswersMap.get(userInput).equals(question.getCorrectAnswer())) {
+                    UserInterface.showMessage("Your answer was correct!\n");
+                    correctAnswers++;
+                } else {
+                    UserInterface.showMessage("The correct answer was " + question.getCorrectAnswer() + "\n");
+                }
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
-            // Check if the answer is correct and respond with appropriate message
-            if (possibleAnswersMap.get(selection).equals(question.getCorrectAnswer())) {
-                System.out.println("Your answer was correct!\n");
-                correctAnswers++;
-            } else {
-                System.out.println("The correct answer was " + question.getCorrectAnswer() + "\n");
-            }
-            questionsAsked++;
-
             // Ask user if he wants to continue to the next question
-            boolean hasNext = questions.size() -1 >= questionsAsked;
+            boolean hasNext = questions.size() - 1 >= questionsAsked;
             while (hasNext) {
-                System.out.println("Would you like to continue to the next question? (y/n)");
-                selection = scanner.nextLine().trim().toLowerCase().charAt(0);
-                if (selection == 'n')
-                    hasNext = false;
-                if (selection == 'y') {
+                UserInterface.showMessage("Would you like to continue to the next question? (Y/N)");
+                char userInput = UserInterface.getUserInput();
+                if (userInput == 'N') hasNext = false;
+                if (userInput == 'Y') {
                     hasNext = true;
                     break;
                 }
             }
             if (!hasNext) break;
         }
-        System.out.printf("You answered %d out of %d correctly! Thank you for playing!\n",
-                correctAnswers, questionsAsked);
-    }
-
-    @Deprecated // This method is replaced with loadQuestions and questions.json
-    public void createSampleQuestions() {
-
-        Question q1 = new Question("Where is Greece?",
-                "Europe",
-                "Asia", "Europe", "America", "Africa");
-
-        Question q2 = new Question("In Greek Mythology, who is the Queen of the Underworld and wife of Hades?",
-                "Persephone",
-                "Artemis", "Athena", "Persephone", "Hera");
-
-        Question q3 = new Question("Which house was Harry Potter almost sorted into?",
-                "Slytherin",
-                "Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin");
-
-        Question q4 = new Question("Which country gifted the Statue of Liberty to the US?",
-                "France",
-                "England", "France", "Canada", "Spain");
-
-        Question q5 = new Question("Which planet is closest to Earth?",
-                "Venus",
-                "Venus", "Mars", "Jupiter", "Mercury");
-
-        this.addQuestions(q1, q2, q3, q4, q5);
+        UserInterface.showMessage("You answered " + correctAnswers + "out of" + questionsAsked + " correctly! Thank you for playing!\n");
     }
 
     public void loadQuestions() {
         ObjectMapper mapper = new ObjectMapper();
         try {
-            this.questions = Arrays.asList(
-                    mapper.readValue(
-                            Paths.get("src/main/java/com/booleanuk/questions.json").toFile(),
-                            Question[].class
-                    ));
+            this.questions = Arrays.asList(mapper.readValue(Paths.get("src/main/java/com/booleanuk/questions.json").toFile(), Question[].class));
         } catch (Exception e) {
             e.printStackTrace();
         }
---
+++ src/main/java/com/booleanuk/UserInterface.java
@@ -0,0 +1,18 @@
+package com.booleanuk;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    // @Description: this class was created in an effort to make quiz classs
+    // independent of the UI, so possibly we can change from console to a different
+    // way to communicate with user in the future, or in different implementations
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static char getUserInput() {
+        return scanner.nextLine().trim().toUpperCase().charAt(0);
+    }
+
+    public static void showMessage(String message) {
+        System.out.println(message);
+    }
+}
