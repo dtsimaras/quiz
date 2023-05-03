1f717948accbad74740b03ad1a170ac23ad17a3b
dtsimaras <tsimaras.dimitrios@gmail.com>
2023-05-03 10:20:55 +0300

    Added a TODO for readme creation

--- src/main/java/com/booleanuk/Main.java
+++ src/main/java/com/booleanuk/Main.java
@@ -2,7 +2,7 @@ package com.booleanuk;
 
 public class Main {
     public static void main(String[] args) {
-        //TODO: add unit tests
+        //TODO: add unit tests, create a readme
         Quiz quiz = Quiz.getInstance();
         quiz.run();
     }
